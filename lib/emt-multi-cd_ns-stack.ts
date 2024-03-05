62ae7e6c892921ea0502fac00696df1b419f063f
Kuu Miyazaki <miyazaqui@gmail.com>
2024-03-06 00:58:35 +0900

    Initial commit

---
+++ lib/emt-multi-cd_ns-stack.ts
@@ -0,0 +1,92 @@
+import * as cdk from 'aws-cdk-lib';
+import { AwsCustomResource, AwsCustomResourcePolicy, PhysicalResourceId } from 'aws-cdk-lib/custom-resources';
+import { Construct } from 'constructs';
+import { MediaTailorWithCloudFront, CloudFront } from 'awscdk-mediatailor-cloudfront-construct';
+
+export class EmtMultiCdNsStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    /*
+    const videoContentSourceUrl = new cdk.CfnParameter(this, "contentSourceUrl", {
+      type: "String",
+      description: "The URL of the MediaTailor HLS endpoint used by MediaTailor as the content origin."
+    }).valueAsString;
+
+    const adDecisionServerUrl = new cdk.CfnParameter(this, "adDecisionServerUrl", {
+      type: "String",
+      description: "The URL of the ad server used by MediaTailor as the ADS."
+    }).valueAsString;
+
+    const slateAdUrl = new cdk.CfnParameter(this, "slateAdUrl", {
+      type: "String",
+      description: "The URL of the video file used by MediaTailor as the slate."
+    }).valueAsString;
+    */
+
+    const videoContentSourceUrl = 'https://xxx.mediapackage.ap-northeast-1.amazonaws.com/out/v1/yyy/index.m3u8';
+
+    const adDecisionServerUrl = 'https://my-ad-server/vast';
+
+    const slateAdUrl = 'https://my-bucket.s3.amazonaws.com/slate.mp4';
+
+    // Create MediaTailor with CloudFront
+    const {cf, emt} = new MediaTailorWithCloudFront(this, 'MediaTailorWithCloudFront', {
+      videoContentSourceUrl,
+      adDecisionServerUrl,
+      slateAdUrl,
+    });
+
+    const innerCfDomainName = cf.distribution.distributionDomainName;
+    const contentPath = cdk.Fn.select(1, cdk.Fn.split('/out/', emt.config.videoContentSourceUrl));
+    const mediaTailorHlsPath = cdk.Fn.select(1, cdk.Fn.split('/v1/', emt.config.attrHlsConfigurationManifestEndpointPrefix));
+
+    // Create another CloudFront
+    const outerCf = new CloudFront(this, 'CloudFront', {
+      videoContentSourceUrl: `https://${innerCfDomainName}/out/${contentPath}`,
+      mediaTailorEndpointUrl: `https://${innerCfDomainName}/v1/${mediaTailorHlsPath}`,
+    });
+
+    const outerCfDomainName = outerCf.distribution.distributionDomainName;
+
+    // Create a Custom Resource to update MediaTailor's CDN configuration with the OuterCloudFront
+    new AwsCustomResource(this, 'AssociateMediaTailorWithCloudFront', {
+      onCreate: {
+        service: 'MediaTailor',
+        action: 'PutPlaybackConfiguration',
+        region: cdk.Aws.REGION,
+        parameters: {
+          Name: emt.config.name,
+          VideoContentSourceUrl: emt.config.videoContentSourceUrl,
+          AdDecisionServerUrl: emt.config.adDecisionServerUrl,
+          SlateAdUrl: emt.config.slateAdUrl,
+          CdnConfiguration: {
+            AdSegmentUrlPrefix: `https://${outerCfDomainName}`,
+            ContentSegmentUrlPrefix: `https://${outerCfDomainName}/out/${contentPath}`,
+          },
+        },
+        physicalResourceId: PhysicalResourceId.of(Date.now().toString()),
+      },
+      //Will ignore any resource and use the assumedRoleArn as resource and 'sts:AssumeRole' for service:action
+      policy: AwsCustomResourcePolicy.fromSdkCalls({
+        resources: AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+    });
+
+    const hlsPlaybackPrefix =`https://${outerCfDomainName}/v1/${mediaTailorHlsPath}`;
+
+    // Output MediaTile HLS playback prefix via CloudFront
+    new cdk.CfnOutput(this, "HLSPlaybackPrefix", {
+      value: hlsPlaybackPrefix,
+      exportName: cdk.Aws.STACK_NAME + "HLSPlaybackPrefix",
+      description: "The HLS playback prefix for MediaTailor Configuration",
+    });
+
+    // Output MediaTailor Session Initialization Prefix
+    new cdk.CfnOutput(this, "SessionInitializationPrefix", {
+      value: emt.config.attrSessionInitializationEndpointPrefix || '',
+      exportName: cdk.Aws.STACK_NAME + "SessionInitializationPrefix",
+      description: "The session initialization prefix for MediaTailor Configuration",
+    });
+  }
+}
---
+++ test/emt-multi-cd_ns.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EmtMultiCdNs from '../lib/emt-multi-cd_ns-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/emt-multi-cd_ns-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EmtMultiCdNs.EmtMultiCdNsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
